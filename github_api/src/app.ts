4e25742ef8052db043322ddaff4b460bb5838f1b
Gerardo Villegas <villegasgerardo.j@gmail.com>
2022-03-01 14:57:00 -0500

    add express config for api

---
+++ github_api/src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/api", (req: Request, res: Response) => {
+  res.send("Welcome to the github api :)");
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+const router = express.Router();
+app.use("/api", router);
+
+export default app;
