6bee54d4a38a117d191517c07230e1db6ae755f8
Gerardo Villegas <villegasgerardo.j@gmail.com>
2022-03-02 12:29:16 -0500

    declare apiHost for flavor staging and production

--- github_api_app/lib/main_production.dart
+++ github_api_app/lib/main_production.dart
@@ -6,7 +6,7 @@ import 'package:github_api_app/injection/injection.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   AppConfig.instance.init(
-    apiHost: '',
+    apiHost: 'https://vercel-express-ts-liart.vercel.app/api',
   );
   await init();
   runApp(const GitHubApiApp());
